5db59c96d009d33074a5d06f73451679d4b20a4b
Alex Xu <xualex008@gmail.com>
2025-12-29 13:05:19 -0500

    bug fix and eslint migration

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Smoke Aware",
   description: "Created by Alex Xu",
+  
 };
 
 export default function RootLayout({ children }) {
